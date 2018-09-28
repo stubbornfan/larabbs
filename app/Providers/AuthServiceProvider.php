f1054d2916448b06fb2a2af6f03acaf80d166d83
stubbornfan <755077098@qq.com>
2018-09-28 07:31:17 +0000

    Add manage contents permission and visit horizon dashboard permission

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -29,5 +29,10 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         //
+        \Horizon::auth(function ($request) {
+            // 是否是站长
+            return \Auth::user()->hasRole('Founder');
+        });
+
     }
 }
